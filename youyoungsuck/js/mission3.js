ae3f07b308a131d1cf77e2a040ae3d2151b0c9fe
bm004387 <bm004387@gmail.com>
2022-11-18 17:17:42 +0900

    js commit

--- youyoungsuck/js/mission3.js
+++ youyoungsuck/js/mission3.js
@@ -1,26 +1,7 @@
 $(document).ready(function(){
-    // $(function($){
-    //     var article = $('.FAQ>.FAQ1>.FAQ2>ul>li');
-    //     article.find('.box').hide();
-    //     $('.FAQ>.article').click(function(){
-            
-    //         var myArticle = $(this).parents('.article:first');
-    //         if(myArticle.hasClass('hide')){
-              
-    //             article.addClass('hide').removeClass('show');
-    //             article.find('.box').slideUp(100);
-    //             myArticle.removeClass('hide').addClass('show');
-    //             myArticle.find('.box').slideDown(100);
-    //         } else{
-    //             myArticle.removeClass('show').addClass('hide');
-    //             myArticle.find('.box').slideUp(100);
-    //         }
-    //         // console.log(".box");
-    //         return false;
-    //     });
-    var article = $('.faq>.faqBody>.article');
+     var article = $('.faq>.faqBody>.article');
     article.find('.a').hide();
-    $('.faq>.faqBody>.article>.q>a').click(function(){
+    $('.faq>.faqBody>.article>.q').click(function(){
         var myArticle = $(this).parents('.article:first');
         if(myArticle.hasClass('hide')){
             article.addClass('hide').removeClass('show');
